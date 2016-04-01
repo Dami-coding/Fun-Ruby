0e1dcf9a195d5f3db666e3ad885ceee1e630a391
dami <boldsdee@aol.com>
2016-04-01 21:18:21 +0100

    Looking at Inheritance

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Fun-Ruby
-Another Insight look at Ruby on rails
+Another Insight look at Ruby on rails.
+Just to keep in touch with my ruby on rail language. I have set up this project and regain some confidence in my Ruby language.
+<br />
+
+
+## Title Topics  
+Will be building some popular ruby and rails project from scratch and learn how to impliment it in different ways for different devices.
+
+ 
--- ruby_projects/user.rb
+++ ruby_projects/user.rb
@@ -1,23 +1,46 @@
 class User
 
   attr_accessor :name, :email
-  def initialize(name, email)
+  def initialize(name, email )
     @name    = name
     @email   = email
   end 
 
   def run
-    puts "Hey, I'm running"
+    puts "Hey, I am running"
   end
+
+end
+
+class Buyer < User
+  puts "hey I'm not running today, apologises"
+end
+
+class Seller < User
+
+end
+
+class Admin < User
 
 end
 
 user  = User.new("Dami", "dami@aol.com")
 
-user.name = "Michael"
+puts "My username is #{user.name} and my email is #{user.email}"
+user.name = "Mike"
 user.email = "mike@aol.com"
-puts user.name
-puts user.email
+
+buyer1 = Buyer.new("Emman", "emmanni@aol.com")
+buyer1.run
+
+seller1 = Seller.new("Lizzie", "damag@aol.com")
+seller1.run
+
+admin = Admin.new("Ekeziel", "zekky@aol.com")
+admin.run
+
+puts Buyer.ancestors
+
 
 
 
